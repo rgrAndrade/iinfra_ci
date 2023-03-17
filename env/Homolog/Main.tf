769bf1c501506c54a19dc745b30e5e320202cbad
Rogério Andrade <104234513+rgrAndrade@users.noreply.github.com>
2023-03-17 20:02:15 -0300

    Update Main.tf

--- env/Homolog/Main.tf
+++ env/Homolog/Main.tf
@@ -1,7 +1,7 @@
 module "homolog" {
     source = "../../infra"
 
-    nome_repositorio = "homolog"
+    nome_repositorio = "main"
     cargoIAM = "homolog"
     ambiante = "homolog"
     imagem="rgrandrade/ci_cd_testes_automatizados:main"
