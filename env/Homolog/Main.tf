e5b4be3b868edb0983c2cc0be6647fad6f0f38b8
Rogério Andrade <rgrandrade@bestdev.top>
2023-03-19 12:40:43 -0300

    config

--- env/Homolog/Main.tf
+++ env/Homolog/Main.tf
@@ -1,9 +1,9 @@
 module "homolog" {
     source = "../../infra"
 
-    nome_repositorio = "main"
-    cargoIAM = "main"
-    ambiante = "main"
+    nome_repositorio = "homolog"
+    cargoIAM = "homolog"
+    ambiante = "homolog"
     imagem="rgrandrade/ci_cd_testes_automatizados:main"
 }
 
